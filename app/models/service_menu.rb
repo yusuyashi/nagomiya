5df6aff8b42972b0c8b1eae5661421ff0765e2c3
yusuyashi <135631605+yusuyashi@users.noreply.github.com>
2023-09-07 15:28:18 +0000

    first commit

---
+++ app/models/service_menu.rb
@@ -0,0 +1,2 @@
+class ServiceMenu < ApplicationRecord
+end
---
+++ db/migrate/20230907151939_create_service_menus.rb
@@ -0,0 +1,10 @@
+class CreateServiceMenus < ActiveRecord::Migration[6.1]
+  def change
+    create_table :service_menus do |t|
+      t.string :course_name, null: false
+      t.integer :price_incl_tax, null: false
+
+      t.timestamps
+    end
+  end
+end
