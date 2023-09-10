7ef22c3c5c3c3090a31ec611a59b69c1699d03bd
yusuyashi <135631605+yusuyashi@users.noreply.github.com>
2023-09-10 15:13:22 +0000

    ゲストログイン機能作成

---
+++ app/models/customer.rb
@@ -0,0 +1,24 @@
+class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  def self.guest
+  find_or_create_by!(email: 'guest@example.com') do |customer|
+    customer.password = SecureRandom.urlsafe_base64
+    customer.last_name = 'ゲスト'
+    customer.first_name = 'ユーザー'
+    customer.last_name_kana = 'ゲスト'
+    customer.first_name_kana = 'ユーザー'
+    customer.postal_code = '1234567'
+    customer.address = 'ゲストアドレス'
+    customer.telephone_number = '1234567890'
+    customer.is_deleted = false
+    # 他の必要なカラムもここに追加
+  end
+  end
+
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  
+}
+  
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
+namespace :public do
+root to: "homes#top"
+end
+
+devise_scope :customer do
+    post 'public/guest_sign_in', to: 'public/sessions#guest_sign_in'
+end
+
+namespace :admin do
+root to: "homes#top"
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
