b6b3d03a14dd431572424a225c7319787387d74f
yusuyashi <135631605+yusuyashi@users.noreply.github.com>
2023-10-05 12:48:48 +0000

    サービスメニューの修正

--- db/migrate/20230914025521_add_description_to_service_menus.rb
+++ db/migrate/20230914025521_add_description_to_service_menus.rb
@@ -1,5 +1,5 @@
 class AddDescriptionToServiceMenus < ActiveRecord::Migration[6.1]
   def change
-    add_column :service_menus, :description, :text, null: false
+    #add_column :service_menus, :description, :text, null: false
   end
 end
