ff234bbc6fbfedc001da1b5d9713189cb0b9985c
yusuyashi <135631605+yusuyashi@users.noreply.github.com>
2023-10-06 07:33:19 +0000

    ゲストログインの本番修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,18 +12,20 @@ class Customer < ApplicationRecord
 
 
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |customer|
-        customer.password = SecureRandom.urlsafe_base64
-        customer.last_name = 'ゲスト名を入力'
-        customer.first_name = 'ゲスト名を入力'
-        customer.last_name_kana = 'ゲストカナを入力' # この行を追加
-        customer.postal_code = '郵便番号を入力'
-        customer.address = '住所を入力'
-        customer.telephone_number = '電話番号を入力'
-        customer.is_deleted = false
-        customer.is_guest = true
-    end
+  find_or_create_by!(email: 'guest@example.com') do |customer|
+    customer.password = SecureRandom.urlsafe_base64
+    customer.last_name = 'ゲスト名を入力'
+    customer.first_name = 'ゲスト名を入力'
+    customer.last_name_kana = 'ゲストカナを入力' # すでに追加されている行
+    customer.first_name_kana = 'ゲストカナを入力' # この行を追加
+    customer.postal_code = '郵便番号を入力'
+    customer.address = '住所を入力'
+    customer.telephone_number = '電話番号を入力'
+    customer.is_deleted = false
+    customer.is_guest = true
   end
+  end
+
 
 
  
@@ -50,12 +52,14 @@ private
   if last_name == 'ゲスト名を入力' || last_name.blank? ||
      first_name == 'ゲスト名を入力' || first_name.blank? ||
      last_name_kana == 'ゲストカナを入力' || last_name_kana.blank? || # この行を追加
+     first_name_kana == 'ゲストカナを入力' || first_name_kana.blank? || # この行を追加
      postal_code == '郵便番号を入力' || postal_code.blank? ||
      address == '住所を入力' || address.blank? ||
      telephone_number == '電話番号を入力' || telephone_number.blank?
     errors.add(:base, "ゲストとしての情報を正しく更新してください。")
   end
   end
+
 
 
 
