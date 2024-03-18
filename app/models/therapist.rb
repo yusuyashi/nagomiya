6d888e702b49c1340dbdd3641935e517b22ed6d9
yusuyashi <135631605+yusuyashi@users.noreply.github.com>
2024-03-18 13:47:43 +0000

    セラピスト新規登録にバリデーション追加

--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -1,4 +1,5 @@
 class Therapist < ApplicationRecord
+  validates :therapist_name, :experience_years, :introduction, presence: true
   has_one_attached :image
   has_many :reservations
   has_many :reservation_details
