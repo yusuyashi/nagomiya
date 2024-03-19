452d8bdf125f3b6490979f4547c9055e1ac302b5
yusuyashi <135631605+yusuyashi@users.noreply.github.com>
2024-03-19 14:35:56 +0000

    施術メニュー新規登録にバリデーション追加

--- app/models/service_menu.rb
+++ app/models/service_menu.rb
@@ -1,4 +1,5 @@
 class ServiceMenu < ApplicationRecord
+   validates :course_name, :price_incl_tax, :description, presence: true
    has_one_attached :image
    has_many :reservations
    has_many :reservation_details
