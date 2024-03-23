68f324f588d957550424010ea9d247b514ab9b78
yusuyashi <135631605+yusuyashi@users.noreply.github.com>
2024-03-23 13:35:41 +0000

    過去の日時で予約できないようにした

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,15 @@
 class Reservation < ApplicationRecord
+  
   belongs_to :service_menu
   belongs_to :therapist, optional: true  # optional: true を追加
   belongs_to :customer
+  
+  validate :visit_datetime_in_future, on: :create
+
+  private
+
+  def visit_datetime_in_future
+    errors.add(:visit_datetime, "過去の日時では予約できません。") if visit_datetime.present? && visit_datetime < DateTime.now
+  end
+  
 end
